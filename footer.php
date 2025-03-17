2cb2f00256045f6386acefc7ea61ad807c3dc4ce
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-17 15:39:48 -0400

    Modifications de l'organisation des éléments dans le footer.php.

--- footer.php
+++ footer.php
@@ -7,22 +7,31 @@
                     "container" => "nav",
                 )); ?>
             </div>
+        </section>
+
+        <section class="piedpage__s2">
+            <div class="footer__contact">
+                Téléphone: <?php echo esc_html(get_theme_mod('footer_telephone', '514-254-7131')); ?>
+            </div>
             <div class="piedpage__s1__adresse">
                 <div class="piedpage__s1__adresse__coord">
                     <?php echo esc_html(get_theme_mod('footer_adresse', '5800 Sherbrooke-est - Montréal (Québec) H1X 2A2')); ?>
-                </div>
-                <div class="piedpage__s1__adresse__recherche">
-                    <?php get_search_form(); ?>
                 </div>
             </div>
             <div class="piedpage__s1__description">
                 <?php echo esc_html(get_theme_mod('footer_mission', 'Voyager pour vivre!')); ?>
             </div>
         </section>
-        <section class="piedpage__s2"></section>
+
         <section class="piedpage__s3">
-            <div class="footer__contact">
-                Téléphone: <?php echo esc_html(get_theme_mod('footer_telephone', '514-254-7131')); ?>
+
+            <div class="footer_menu_nav">
+                <?php wp_nav_menu(array(
+                    'menu' => 'principal',
+                    'container' => 'nav',
+                    'container_class' => 'footer_menu_nav'
+                )); ?>
+                     <?php get_search_form(); ?>
             </div>
         </section>
     </div>
