10a4f0f3e483669522d5cc131419c32f92aea9d6
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-19 00:17:59 -0400

    TP1 - Complet.

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -5,23 +5,31 @@
  * TEMPLATE-PART CARTE.
  * 
  * Voici mon modèle de carte, un gabarit contenant les cartes présentant les articles de voyage.
+ * L'étoile uniquement si l'article appartient à la catégorie populaire.
  */
 ?>
 
 <article class="carte carte--grande">
   <figure class="carte__image">
-    </figure>
+  <?php 
+      if (has_term('populaire', 'category')) { 
+        echo '<span class="carte__etoile">&#9733;</span>';
+      }
+    ?>
+  </figure>
+  
   <div class="carte__contenu">
     <?php
         if (has_post_thumbnail()) {
-        the_post_thumbnail('thumbnail'); }
+            the_post_thumbnail('thumbnail'); 
+        }
     ?>    
     <h4 class="carte__titre"><?php the_title(); ?></h4>
-    <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
+    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, " ... "); ?></p>
     <p>Température Maximum: <?php the_field("temperature_maximum"); ?>°C</p>
     <p>Température Minimum: <?php the_field("temperature_minimum"); ?>°C</p>
     <p>Température Moyenne: <?php the_field("temperature_moyenne"); ?>°C</p>
     <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite</a>
-   <?php the_category()?>
+    <?php the_category(); ?>
   </div>
 </article>
