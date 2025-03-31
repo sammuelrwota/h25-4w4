2782e989112cc7fd94f4c20cba22c506dc3f245a
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-31 15:55:10 -0400

    Ajout du menu et son conteneur dans le fichier section_404.php.

--- section_404.php
+++ section_404.php
@@ -5,6 +5,12 @@
   <h1 class="H1Erreur404">Oops, vous avez échoué sur l'île 404 !</h1>
   <h2 class="H2Erreur404">Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</h2>
   <a href="<?php echo home_url(); ?>" class="btn-home">Retour à l'accueil</a>
+  <div class="404_menu">
+        <?php wp_nav_menu(array(
+            "menu" => "menu404",
+            "container" => "nav",
+        )); ?>
+  </div>
   </div>
 </section>
     <?php get_footer(); ?>
