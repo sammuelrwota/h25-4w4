10dd907d9b437e652c12b23d0ac1319a4f11f636
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-05-19 16:08:54 -0400

    TP2 deuxième commit : mise en place initiale de l’interface pour les articles individuels (single post).

--- single-post.php
+++ single-post.php
@@ -1,24 +1,28 @@
 <?php get_header(); ?>
 
     <section class="populaire">
-        <div class="global">
-      
+        <div class="global"> 
+             <h2 class="singlepost__titre"><?php the_title(); ?></h2>
+            <div class="singlepost">
+               
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article>
+            <article class="singlepost__article">
             <?php
                 if (has_post_thumbnail()) {
-                the_post_thumbnail('large'); }
+                the_post_thumbnail('medium'); }
             ?>  
-                <h2><?php the_title(); ?></h2>
-                <div><?php the_content() ?>
+               
+                <div class="singlepost__div"><?php the_content() ?>
                 <?php the_category(); ?>
                 <?php  $tableau = get_the_category(); 
                 // print_r ($tableau);
                 ?>
+                <div class="singlepost__temperatures">
                 <p>Température maximum: <?php the_field('temperature_maximum') ?>&#176;C</p>
                 <p>Température minimum: <?php the_field('temperature_minimum') ?>&#176;C</p>
-                <p>Température moyenne: <?php the_field('temperature_moyenne') ?>&#176;C</p>
+                <p>Température moyenne: <?php the_field('temperature_moyenne') ?>&#176;C</p></div>
             <?php endwhile; endif; ?>
+            </div>
         </div>
     </section>
     <?php get_footer(); ?>
