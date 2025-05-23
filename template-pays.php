c5d0a7803f9699b4fdcc1864101d2e24a00ba59c
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-05-23 11:55:19 -0400

    Épreuve final SR : Affichage de templates.

--- template-pays.php
+++ template-pays.php
@@ -11,7 +11,11 @@
 
         <section class="galerie">
             <div class="global">
-            
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <div><?php the_content() ?>
+        <?php endwhile; endif; ?>
                
             </div>
         </section>
